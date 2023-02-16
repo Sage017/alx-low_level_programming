2bc5d3b7aa6781d8d0d3006f8bfd698bfcfc01ce
Mubaraq Babatunde <Dreynibee@gmail.com>
2023-02-16 18:41:24 +0100

    Add: project files

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the string puz
+ *
+ * Return: 0 for success, 1 otherwise
+ */
+int main(void)
+{
+	char puz[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(puz);
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the string out
+ *
+ * Return: 0 for success, 1 otherwise
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
